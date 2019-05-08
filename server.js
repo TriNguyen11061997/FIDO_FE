152cfcc4c037aa43addd0ed83e11864e86c96175
ADMIN\Trí Nguyễn <ntntri.it1106@gmail.com>
2019-05-08 13:06:09 +0700

    tringuyen

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fidofe'));
+app.use(express.static(__dirname + '/dist/FIDO_FE'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/fidofe/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/FIDO_FE/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
